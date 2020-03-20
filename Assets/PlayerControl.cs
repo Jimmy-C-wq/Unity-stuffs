7feba7a2a39878753942809d45ca6464c94f12e2
Jimmy-C-wq <62439814+Jimmy-C-wq@users.noreply.github.com>
2020-03-20 16:53:35 +0000

    Add files via upload

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerControl: NetworkBehaviour {
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+
+        controller.Move(move* speed * Time.deltaTime);
+    }
+}
